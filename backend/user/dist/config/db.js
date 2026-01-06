731dfdeaa4a95a9d4969019f75f7d70d8a3d3aa1
purunsingh07 <purunsingh07@gmail.com>
2026-01-06 18:16:17 +0530

    new app

---
+++ backend/user/dist/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+export const connectDB = async () => {
+    const url = process.env.MONGO_URL;
+    if (!url) {
+        throw new Error("MONGO_URL is not defined in environment variables");
+    }
+    try {
+        await mongoose.connect(url, {
+            dbName: "ChatAppmicroserviceapp"
+        });
+        console.log("Connected to MongoDB successfully");
+    }
+    catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+        process.exit(1);
+    }
+};
+export default connectDB;
+//# sourceMappingURL=db.js.map

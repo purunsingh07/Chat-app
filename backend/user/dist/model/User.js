6b956ac24bb198adf1fe90c9e435fef239aee66b
purunsingh07 <purunsingh07@gmail.com>
2026-01-08 14:24:36 +0530

    model done

--- backend/user/dist/model/User.js
+++ backend/user/dist/model/User.js
@@ -1,1 +1,15 @@
-import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema, SchemaType } from "mongoose";
+const schema = new Schema({
+    name: {
+        type: String,
+        requires: true,
+    },
+    email: {
+        type: String,
+        requires: true,
+        unique: true,
+    }
+}, {
+    timestamps: true,
+});
+export const User = mongoose.model("User", schema);
--- backend/user/src/model/User.ts
+++ backend/user/src/model/User.ts
@@ -1,1 +1,22 @@
-import mongoose,{Document,Schema} from "mongoose";
+import mongoose,{Document,Schema, SchemaType} from "mongoose";
+
+export interface IUser extends Document{
+    name: string;
+    email: string;
+}
+
+const schema: Schema<IUser> = new Schema({
+    name:{
+        type: String,
+        requires: true,
+    },
+    email:{
+        type: String,
+        requires: true,
+        unique: true,
+    }
+},{
+    timestamps: true,
+});
+
+export const User = mongoose.model<IUser>("User",schema);

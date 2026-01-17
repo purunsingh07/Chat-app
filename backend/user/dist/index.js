147d50bc793eaa23a70856794a75084c12fdb2f3
purunsingh07 <purunsingh07@gmail.com>
2026-01-17 16:01:57 +0530

    controller and routes of user is developing

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -1,9 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { createClient } from 'redis';
+import userRoutes from './routes/user.js';
 dotenv.config();
 connectDB();
+export const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+redisClient.connect().then(() => console.log("connected to redis"))
+    .catch(console.error);
 const app = express();
+app.use("api/v1", userRoutes);
 const port = process.env.PORT;
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
